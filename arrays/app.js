7c765db8b188715e7bd886c47e8f1838293bc7db
Guillermo García <guillermieres97@gmail.com>
2022-11-14 12:57:28 +0100

    new option for this exercise

--- arrays/app.js
+++ arrays/app.js
@@ -1,11 +1,18 @@
 "use strict";
 
 /* given an array of fruits, remove the fruits that finish with the E letter */
-const fruits = ["orange", "banana", "apple", "strawberry"];
+const fruits1 = ["orange", "banana", "apple", "strawberry"];
+const fruits2 = ["orange", "banana", "apple", "strawberry"];
 
-fruits.forEach((fruit, index) => {
+fruits1.forEach((fruit, index) => {
   const lastLetter = fruit.length - 1;
+
   if (fruit[lastLetter] === "e") {
-    fruits.splice(index, 1);
+    fruits1.splice(index, 1);
   }
 });
+
+const newArray = fruits2.filter(fruit => fruit[fruit.length - 1] != "e");
+
+console.log(fruits1);
+console.log(newArray);
